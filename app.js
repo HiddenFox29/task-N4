652fcb29ea727fbe4f266138dfab27cb774e0961
Aleksandr Alekseev <hiddenfox29@yandex.ru>
2019-09-29 10:50:39 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+fname   = "Александр";
+lname   = "Алексеев";
+age     = 18;
+country = "Казахстане";
+lang    = "JavaScript";
+hi      = "Привет!!"
+
+alert('Совершеннолетие в ' + country + ' наступает в ' + age)
+alert(hi + ' меня зовут ' + fname + ' '+ lname + ' и я изучаю ' + lang)
